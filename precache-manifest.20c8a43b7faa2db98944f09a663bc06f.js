f8eba4e09a768ffd77928bd9cded32ce86a68eb9
David <davidringborg@gmail.com>
2020-09-10 10:23:51 +0200

    Updates

---
+++ precache-manifest.20c8a43b7faa2db98944f09a663bc06f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d510e8e25be22f5e2f97addf9bcb37ef",
+    "url": "/WishListApp/index.html"
+  },
+  {
+    "revision": "fdcf3cbb5af50d59977a",
+    "url": "/WishListApp/static/css/main.a4bc8624.chunk.css"
+  },
+  {
+    "revision": "9803a663ddcbe728fe37",
+    "url": "/WishListApp/static/js/2.f512ff8f.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/WishListApp/static/js/2.f512ff8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdcf3cbb5af50d59977a",
+    "url": "/WishListApp/static/js/main.2e72e77b.chunk.js"
+  },
+  {
+    "revision": "0b985198f227e2eacf59",
+    "url": "/WishListApp/static/js/runtime-main.f78bbc2b.js"
+  }
+]);
