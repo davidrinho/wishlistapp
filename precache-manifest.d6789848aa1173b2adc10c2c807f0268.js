816a25838570f9146d115f6962da3511f3e483ee
David <davidringborg@gmail.com>
2020-09-10 10:47:30 +0200

    Updates

---
+++ precache-manifest.d6789848aa1173b2adc10c2c807f0268.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2561d70205068264e5fe1879a33c289b",
+    "url": "/wishlistapp/index.html"
+  },
+  {
+    "revision": "5c7a12f61cf6bf3c8390",
+    "url": "/wishlistapp/static/css/main.a4bc8624.chunk.css"
+  },
+  {
+    "revision": "3cd6cebe0cbbd2b9fdb6",
+    "url": "/wishlistapp/static/js/2.6c3e6d03.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/wishlistapp/static/js/2.6c3e6d03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c7a12f61cf6bf3c8390",
+    "url": "/wishlistapp/static/js/main.4d052921.chunk.js"
+  },
+  {
+    "revision": "f09385d27f10115f01b5",
+    "url": "/wishlistapp/static/js/runtime-main.f0de20fd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/WishListApp/precache-manifest.8bbb0a7c65e400c0223c00a1d448c0c7.js"
+  "/wishlistapp/precache-manifest.d6789848aa1173b2adc10c2c807f0268.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/WishListApp/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/wishlistapp/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
